ce28611b63cbe98325539526abb49c9ef8c35ec7
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-05-07 20:09:32 +0000

    Moved iocsh registration to mar345.cpp
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@8913 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/mar345Src/mar345.cpp
+++ ADApp/mar345Src/mar345.cpp
@@ -32,14 +32,13 @@
 #include <epicsStdio.h>
 #include <epicsMutex.h>
 #include <cantProceed.h>
+#include <iocsh.h>
+#include <epicsExport.h>
 
 #include <asynOctetSyncIO.h>
 
-#include "ADStdDriverParams.h"
-#include "NDArray.h"
 #include "ADDriver.h"
 
-#include "drvMAR345.h"
 #include "mar3xx_pck.h"
 
 /** Messages to/from server */
@@ -803,3 +802,32 @@ mar345::mar345(const char *portName, const char *serverPort,
         return;
     }
 }
+
+/* Code for iocsh registration */
+static const iocshArg mar345ConfigArg0 = {"Port name", iocshArgString};
+static const iocshArg mar345ConfigArg1 = {"server port name", iocshArgString};
+static const iocshArg mar345ConfigArg2 = {"maxBuffers", iocshArgInt};
+static const iocshArg mar345ConfigArg3 = {"maxMemory", iocshArgInt};
+static const iocshArg mar345ConfigArg4 = {"priority", iocshArgInt};
+static const iocshArg mar345ConfigArg5 = {"stackSize", iocshArgInt};
+static const iocshArg * const mar345ConfigArgs[] =  {&mar345ConfigArg0,
+                                                     &mar345ConfigArg1,
+                                                     &mar345ConfigArg2,
+                                                     &mar345ConfigArg3,
+                                                     &mar345ConfigArg4,
+                                                     &mar345ConfigArg5};
+static const iocshFuncDef configMAR345 = {"mar345Config", 6, mar345ConfigArgs};
+static void configMAR345CallFunc(const iocshArgBuf *args)
+{
+    mar345Config(args[0].sval, args[1].sval, args[2].ival,
+                 args[3].ival, args[4].ival, args[5].ival);
+}
+
+
+static void mar345Register(void)
+{
+
+    iocshRegister(&configMAR345, configMAR345CallFunc);
+}
+
+epicsExportRegistrar(mar345Register);
