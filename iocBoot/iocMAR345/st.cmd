fff9eb339552440905871b43eff9a05f27839b4d
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-05-28 18:42:01 +0000

    Changed SIZE macro to TYPE in NDStdArrays.template
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@8964 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocMAR345/st.cmd
+++ iocBoot/iocMAR345/st.cmd
@@ -23,7 +23,7 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/mar345.template","P=13MAR345_1:,R=cam1:
 # Create a standard arrays plugin
 NDStdArraysConfigure("MARImage", 5, 0, "MAR", 0, -1)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13MAR345_1:,R=image1:,PORT=MARImage,ADDR=0,TIMEOUT=1,NDARRAY_PORT=MAR,NDARRAY_ADDR=0")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=13MAR345_1:,R=image1:,PORT=MARImage,ADDR=0,TIMEOUT=1,SIZE=16,FTVL=SHORT,NELEMENTS=12000000")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=13MAR345_1:,R=image1:,PORT=MARImage,ADDR=0,TIMEOUT=1,TYPE=Int16,FTVL=SHORT,NELEMENTS=12000000")
 
 # Create an ROI plugin
 NDROIConfigure("MARROI", 5, 0, "MAR", 0, 4, 20, -1)
